70386ee386d36220eea6223adf9973e6eb618bd6
Elijah Korir <elijahkiptoo98@gmail.com>
2022-02-02 11:50:34 +0300

    Mock Open Trivia API

---
+++ src/services/questionService.js
@@ -0,0 +1,24 @@
+export const fetchQuestions = async (category, difficulty) => {
+  const response = await fetch(
+    "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple"
+  );
+  if (response.ok) {
+    const responseBody = await response.json();
+    console.log(responseBody);
+  }
+};
+
+export const mockQuestion = () => {
+  return [
+    {
+      question: "Which of the following was Brazil was a former colony under?",
+      correct_answer: "Portugal",
+      incorrect_answers: ["Spain", "The Netherlands", "France"],
+    },
+    {
+      question: "In 1453, which important city fell?",
+      correct_answer: "Constantinople",
+      incorrect_answers: ["Rome", "Hamburg", "Athens"],
+    },
+  ];
+};
