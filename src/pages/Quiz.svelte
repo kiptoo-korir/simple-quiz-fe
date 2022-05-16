12f77c921d4b5e517632bcaa7ed8fe276a3641df
Elijah Korir <elijahkiptoo98@gmail.com>
2022-05-16 13:06:43 +0300

    Make app to route back to startup page when quiz isn't set up

--- src/pages/Quiz.svelte
+++ src/pages/Quiz.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { onDestroy } from "svelte";
+  import { onDestroy, onMount } from "svelte";
   import Trivia from "../components/Trivia.svelte";
   import Loading from "../components/Loading.svelte";
   import { QuestionStore } from "../stores/questions";
@@ -13,15 +13,19 @@
 
   onDestroy(unsubscribe);
 
-  if (questions === []) {
-    navigate("/");
-  }
+  $: qnLength = questions.length;
+
+  onMount(() => {
+    if (qnLength === 0) {
+      navigate("/");
+    }
+  });
 </script>
 
 <div>
-  {#if questions}
+  {#if qnLength > 0}
     <Trivia realQuestions={questions} />
   {:else}
-    <Loading />
+    {navigate("/")}
   {/if}
 </div>
